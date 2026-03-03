deec99fb724a2bba23cda7401a23e71bd416b7a3
HarmanSangga <Harmansangg@gmail.com>
2026-03-03 16:15:05 +0700

    add animation, update portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,50 @@
 //togle & responsive navigation
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const navLists = document.querySelector("nav");
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
 
-    burger.addEventListener("click", () => {
-        navLists.classList.toggle("nav-active");
-        burger.classList.toggle("toggle-burger");
-    });
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
 };
 
 navSlide();
 
 window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-        form.reset();
-    }
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
 };
 
+document.addEventListener("scroll", function () {
+  const nav = document.querySelector(".nav");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
 
-document.addEventListener('scroll', function () {
-    const nav = document.querySelector('.nav');
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-}); 
+document.addEventListener("DOMContentLoaded", function () {
+  const reveals = document.querySelectorAll(".reveal");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const animation = entry.target.dataset.animate.split(" ");
+
+          entry.target.classList.add("animate__animated", ...animation);
+
+          observer.unobserve(entry.target); // supaya animasi cuma sekali
+        }
+      });
+    },
+    {
+      threshold: 0.2, // muncul ketika 20% terlihat
+    },
+  );
+
+  reveals.forEach((el) => observer.observe(el));
+});
